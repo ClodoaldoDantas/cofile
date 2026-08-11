abc75450d081430b2af940b4bcf7c0c00f3e88d7
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2026-08-10 22:18:06 -0300

    feat: add Epilogue font configuration and apply it in global styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config'
+import { defineConfig, fontProviders } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: 'Epilogue',
+      cssVariable: '--font-epilogue',
+      weights: [400, 500, 600],
+      styles: ['normal'],
+      subsets: ['latin'],
+    },
+  ],
+})
