be5289d694b75c655be211466e09fe1e9c2f2dc2
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2026-08-12 10:23:01 -0300

    feat: add footer component with social media links and update layout to include it

---
+++ src/constants/social-media.ts
@@ -0,0 +1,11 @@
+import instagramIcon from '@/assets/instagram.svg'
+import twitterIcon from '@/assets/twitter.svg'
+import dribbbleIcon from '@/assets/dribbble.svg'
+import mediumIcon from '@/assets/medium.svg'
+
+export const socialMedia = [
+  { name: 'Instagram', href: '/', icon: instagramIcon },
+  { name: 'Twitter', href: '/', icon: twitterIcon },
+  { name: 'Dribbble', href: '/', icon: dribbbleIcon },
+  { name: 'Medium', href: '/', icon: mediumIcon },
+]
