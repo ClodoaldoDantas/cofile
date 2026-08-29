df9bd408828fd3b5051156be6beb309f66ae38a4
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2026-08-29 15:10:25 -0300

    feat: implement dynamic partners section by introducing partners constant and updating Partners component

---
+++ src/constants/partners.ts
@@ -0,0 +1,11 @@
+import papleImg from '@/assets/paple.svg'
+import masbobzImg from '@/assets/masbobz-works.svg'
+import notionImg from '@/assets/notion.svg'
+import superImg from '@/assets/super.svg'
+
+export const partners = [
+  { source: papleImg.src, alt: 'Paple' },
+  { source: masbobzImg.src, alt: 'Masbobz Works' },
+  { source: notionImg.src, alt: 'Notion' },
+  { source: superImg.src, alt: 'Super' },
+]
