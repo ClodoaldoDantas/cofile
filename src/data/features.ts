e89f08dacc5f299e11a459eb9b1556c9ac43a7be
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2026-08-30 00:24:21 -0300

    feat: add Features component and corresponding data for feature display

---
+++ src/data/features.ts
@@ -0,0 +1,19 @@
+export const features = [
+  {
+    icon: 'ph:watch',
+    title: '24 Hours Customer Service',
+    description:
+      'We respect your passion to run your business by always accompany you',
+  },
+  {
+    icon: 'ph:coins',
+    title: 'Currencies All Over The World',
+    description:
+      'Our complete and real-time currencies database will help your business',
+  },
+  {
+    icon: 'ph:arrow-circle-up-right',
+    title: 'Upgrade Once You Ready',
+    description: 'No worries, we always welcome you once you ready to upgrade',
+  },
+]
